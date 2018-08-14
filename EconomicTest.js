795cf76ef6531e81e7fd2b1b1b2c896a5e8e2e9c
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2018-08-14 20:00:23 +0000

    Update EconomicTest.js

--- EconomicTest.js
+++ EconomicTest.js
@@ -1,1 +1,40 @@
 
+/* @License Starts * * Copyright © 2015 - present. MongoExpUser 
+* 
+* License: MIT 
+* 
+* @License Ends 
+* 
+* This module is for testing the EP-Economic functionalities
+*
+*/
+  
+  
+  
+//create module: through creation and execution of named or anonymous function with IIFEvar 
+
+EconomicTest = (function economicTest(){
+{
+
+    'use strict';
+
+    //constructor
+    function main(){var test = undefined} 
+  
+  
+    //add test codes later: IP_PROGRESS
+    main.prototype.testEPeconomich = function (arguments)
+    {
+      //add test codes later: IP_PROGRESS
+      
+    }    
+  
+  
+  
+    //export all prototype functions on main
+    module.exports = main;
+
+    //return main to make its objects accessible
+    return main;
+
+}());
