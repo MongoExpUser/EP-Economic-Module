67e175473785fd1ddaf6a79fd9174b22ff67c460
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-01-28 04:32:20 -0600

    Update EconomicTest.js

--- EconomicTest.js
+++ EconomicTest.js
@@ -5,7 +5,7 @@
 * 
 * @License Ends 
 * 
-* This module is for testing the EP-Economic functionalities in: EconomicModel.js and EconomicCrypto.js
+* This module is for testing the EP-Economic functionalities in: EconomicModel.js 
 *
 */
   
@@ -25,47 +25,6 @@ EconomicTest = (function economicTest(){
       var prodFunction    = [[0, 2, 4, 6], [0, 20, 200, 200]];
       new econModel().economicModel(inputDataActual, prodFunction, "tight", null);
     }
-        
-    main.prototype.testingEconomicCrypto = function ()
-    {
-        var fs                    = require('fs');
-        var path                  = require('path');
-        var econCrypto            = require('EconomicCrypto.js');
-            
-        console.log();
-        console.log('------------Testing Crypto Starts--------------------------');
-        var sigPath1        = 'file-1.png';                                //path of file to hash 
-        var sigPath2        = 'file-2.pdf';                                //path of file to hash
-        var sig1             = fs.readFileSync(sigPath1);                  //file to hash
-        var sig2             = fs.readFileSync(sigPath2);                  //file to hash
-        var sig3             = "MongoExpUser";                             //string to hash
-        var sig              = [sig1, sig2, sig3];                         //array of items to hash
-            
-        var hashAlgorithm1   = 'bcrypt';
-        var hashAlgorithm2   = 'sha512';
-        var hashAlgorithm3   = 'whirlpool';
-        var hashAlgorithm4   = 'scrypt';
-        var toInspect        = econCrypto;
-      
-        console.log(require('util').inspect(toInspect, { showHidden: true, colors: true, depth: 4 })); //print structure: a confirmation test
-               
-        var blockhain        = new econCrypto().isHashConsensus(sig, hashAlgorithm1);
-        var priorBlockchains = [blockhain[1], blockhain[1], blockhain[1]];
-        var compareSig       = sig;
-        var compareSalt      = blockhain[0];
-        var compareHashSig   = priorBlockchains;
-        var compareDateNow   = blockhain[2];
-        var newBlockhain     = new econCrypto().isHashConsensus(sig, hashAlgorithm1, compareSig, compareSalt, compareHashSig, compareDateNow);
-        console.log("chain - salt, hash, time :");
-        console.log(blockhain);
-        console.log();
-        console.log("confirm :");
-        console.log(newBlockhain);
-        console.log()
-        console.log('------------Testing Crypto Ends--------------------------');
-        console.log();
-    }
-  
   
     //export all prototype functions on main
     module.exports = main;
@@ -74,11 +33,9 @@ EconomicTest = (function economicTest(){
     return main;
 }());
   
-  
-    //invoke test as IIFE
-    (function testAll(option)
-    {
+//invoke test as IIFE
+(function testAll(option)
+{
        'use strict';
-       new EconomicTest().testingEconomicModel();   // 1. EconomicModel testing
-       new EconomicTest().testingEconomicCrypto();  // 2. EconomicCrypto testing
-    }());
+       new EconomicTest().testingEconomicModel();   //. EconomicModel testing
+}());
